d630eedad0732bd1660ae8ff303b168ff470bd83
Kerb <hakkerby01@gmail.com>
2024-05-17 15:29:24 +0700

    CHANGED PACKAGE.JSON COMMANDS

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import sass from 'vite-plugin-sass';
-import svgr from "vite-plugin-svgr";
+// import sass from 'vite-plugin-sass';
+// import svgr from "vite-plugin-svgr";
 
  
 export default defineConfig({
-  plugins: [sass(), svgr()],
+  // plugins: [sass(), svgr()],
 });
