3a87f0d9ea66d8a3b3ddb286343c282ac19a8fb8
K3RBY <hakkerby01@gmail.com>
2024-06-07 09:46:10 +0700

    modified: vite.config.js added entry point for mac.html to output into dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 // import sass from 'vite-plugin-sass';
 // import svgr from "vite-plugin-svgr";
+import { resolve } from 'path';
 
- 
 export default defineConfig({
   // plugins: [sass(), svgr()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        mac: resolve(__dirname, "mac.html"),
+        // Add more entry points as needed
+      },
+    },
+  },
 });
+ 
+
